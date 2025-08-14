786f272d8ff37a8a629c06f8674aa8e8238d011d
unknown <foxRainR@gmail.com>
2025-08-14 09:50:07 -0400

    input outline and 4 basic operators

--- src/main.java
+++ src/main.java
@@ -2,8 +2,33 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Accept user input of (number) (operator) (number)
+		// Store numbers into variables
+		// Read which operator was used and jump to the corresponding method
+		
+		int x = //input one
+		int y = //input two
+		
 
 	}
-
+	// Add
+	static int add(int x, int y) {
+		int sum = x + y;
+		return sum;
+	}
+	// Subtract
+	static int sub(int x, int y) {
+		int difference = x - y;
+		return difference;
+	}
+	// Multiply
+	static int mul(int x, int y) {
+		int product = x * y;
+		return product;
+	}
+	// Divide
+	static int div(int x, int y) {
+		int quotient = x / y;
+		return quotient;
+	}
 }
